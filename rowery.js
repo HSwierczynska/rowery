2563f354189d7fc335ca5a7b68e2f1d2a0d64450
nukeop <alsw@protonmail.com>
2017-07-04 20:27:21 +0200

    Main script and styles

---
+++ rowery.js
@@ -0,0 +1,57 @@
+
+
+
+window.onload = function onWindowReady() {
+  fetch('https://app.socialbicycles.com/api/networks/105/hubs?per_page=150')
+  .then(response => response.json())
+  .then(data => {
+    $("#loader").css("opacity", 0);
+
+
+    data.items.map(hub => {
+      var status = hub.available_bikes/hub.racks_amount;
+      var statusClass = 'red-status';
+      if (status >= 0.5) {
+        statusClass = 'green-status';
+      } else if (status >= 0.1) {
+        statusClass = 'yellow-status';
+      }
+
+      $('#rowery > tbody:last-child').append(
+        $('<tr>').append(
+          $('<td>').append(
+            hub.name
+          ),
+          $('<td>').append(
+            hub.available_bikes + '/' + hub.racks_amount
+          )
+        ).addClass(statusClass)
+      );
+    });
+
+  });
+}
+
+
+function whichTransitionEvent(element){
+    var t;
+    var transitions = {
+      'transition':'transitionend',
+      'OTransition':'oTransitionEnd',
+      'MozTransition':'transitionend',
+      'WebkitTransition':'webkitTransitionEnd'
+    }
+
+    for(t in transitions){
+        if( element.style[t] !== undefined ){
+            return transitions[t];
+        }
+    }
+}
+
+/* Listen for a transition! */
+var transitionEvent = whichTransitionEvent(document.getElementById("loader"));
+var e = document.getElementById("loader");
+transitionEvent && e.addEventListener(transitionEvent, function() {
+  e.parentElement.removeChild(e);
+});
